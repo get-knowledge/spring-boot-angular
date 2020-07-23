e12a9d87e1f219f1ca12f379599704d4e05d5c2c
Dhananjay Verma <@dhananjay.verma>
2020-07-23 23:31:19 +0530

    WFH 001 commit

---
+++ src/test/java/com/gobeyond/employee/crud/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobeyond.employee.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
